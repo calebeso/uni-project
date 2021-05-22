fff3f90fe8b9ff6bd7b67adc8aae5e00357ebf69
Calebeso <calebeso045@gmail.com>
2021-05-22 10:04:34 -0300

    feat(Validations Alerts) : creating view to receive validation messages

---
+++ resources/views/includes/alerts.blade.php
@@ -0,0 +1,31 @@
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div><br>
+@endif
+
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+    {{ session('status') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@elseif (session('unsuccess'))
+<div class="alert alert-danger" role="alert">
+    {{ session('unsuccess') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
+
+@if (session('error'))
+<div class="alert alert-danger">
+    {{ session('error') }}
+</div><br>
+@endif
